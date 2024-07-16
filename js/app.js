cf766691d7acdb78a842257ce977ab797200cbf4
Ito Aghayere <itohan.aghayere@gmail.com>
2024-07-16 11:20:35 -0700

    Updated Step 5

--- js/app.js
+++ js/app.js
@@ -9,6 +9,17 @@ let turn
 let winner
 let tie
 
+const winningCombos = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],                      // all the combinations of numbered squares on the grid that would be a tic tac toe
+  ]    
+
 /*------------------------ Cached Element References ------------------------*/
 const squareEls = document.querySelectorAll('.sqr')
 const messageEl = document.getElementById('message')
@@ -19,7 +30,7 @@ init()
 function init() {
     board = ['', '', '', '', 'o', '', 'x', '', '']  // array so use for...each
     turn = 'x'
-    winner = false
+    winner = false                  // changed this to "true" to test if meessages would come up.
     tie = false
     render()
 }
@@ -52,6 +63,9 @@ function updateMessage() {
     }
 
  /*----------------------------- Event Listeners -----------------------------*/
+
+
+ 
 
 //1) Define the required variables used to track the state of the game.
 
