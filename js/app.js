5710ced6df9789081bbcba599632720ff2fa19cb
Ito Aghayere <itohan.aghayere@gmail.com>
2024-07-16 04:37:32 -0700

    Steps 1-3

--- js/app.js
+++ js/app.js
@@ -4,18 +4,41 @@
 
 
 /*---------------------------- Variables (state) ----------------------------*/
-
-
+let board
+let turn
+let winner
+let tie
 
 /*------------------------ Cached Element References ------------------------*/
-
-
+const squareEls = document.querySelectorAll('.sqr')
+const messageEl = document.getElementById('message')
+console.log(messageEl)
 
 /*-------------------------------- Functions --------------------------------*/
+function init() {
+    let board = ['', '', '', '', '', '', '', '', '']
+    turn = 'x'
+    winner = true
+    tie = false
+    render()
+}
 
-
+function render() {
+}
 
 /*----------------------------- Event Listeners -----------------------------*/
 
+//1) Define the required variables used to track the state of the game.
 
+//2) Store cached element references.
 
+//3) Upon loading, the game state should be initialized, and a function should 
+//   be called to render this game state.
+
+//4) The state of the game should be rendered to the user.
+
+//5) Define the required constants.
+
+//6) Handle a player clicking a square with a `handleClick` function.
+
+//7) Create Reset functionality.
